be158cc7f8444cfb46552d178067297be849f231
rashmitha-converj <152256974+rashmitha-converj@users.noreply.github.com>
2024-01-17 15:07:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const http = require('http');
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>In GIT_COMMIT  $$$ App</h1> <h4>Message: Success Done Updating</h4> <p>Version 6.0.0</p>');
- })
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello Node!\n'
+  res.end(msg);
+});
 
-
-app.listen(port, ()=> {
-  console.log(`Demo app is up and listening to port: ${port}`);
-})
- 
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
